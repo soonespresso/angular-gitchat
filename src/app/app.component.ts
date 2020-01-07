aab272b475595b9940cdcd07b59ef0c5d009f836
soonespresso <icanflylzx2012@163.com>
2020-01-07 18:43:42 +0800

    submit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, TemplateRef, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-gitchat';
+export class AppComponent implements AfterViewInit {
+
+  name = 'Darwin';
+
+  @ViewChild('tpl', { static: false })
+  tpl: TemplateRef<any>;
+
+  ngAfterViewInit(): void {
+    console.dir(this.tpl);
+    const embeddedView = this.tpl.createEmbeddedView(null);
+    console.dir(embeddedView);
+  }
 }
