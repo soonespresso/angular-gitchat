4663cfde4d01200ea7c868762e4b983794e4ee9b
soonespresso <icanflylzx2012@163.com>
2020-01-15 13:46:04 +0800

    submit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormQuickStartComponent } from './form-quick-start/form-quick-start.component';
-import { ChineseMobileValidatorDirective } from './directives/chinese-mobile-validator.directive';
-import { EqualValidatorDirective } from './directives/equal-validator.directive';
 import { TemplateDrivenComponent } from './template-driven/template-driven.component';
 import { ConfirmValidatorDirective } from './directives/confirm-validator.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormQuickStartComponent,
-    ChineseMobileValidatorDirective,
-    EqualValidatorDirective,
     TemplateDrivenComponent,
     ConfirmValidatorDirective
   ],
--- src/app/form-quick-start/form-quick-start.model.ts
+++
@@ -1,6 +0,0 @@
-export class RegisterModel {
-  userName: string;
-  password: string;
-  confirmPassword: string;
-  mobile: string;
-}
